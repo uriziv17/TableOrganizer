c3296df8f5237fd7f8affe61a26e57aaca226f74
Uri Ziv <uriziv10@gmail.com>
2024-04-29 14:10:48 +0300

    working on creating spots

--- Spot.py
+++ Spot.py
@@ -1,9 +1,15 @@
+import math
+
+
 class Spot(object):
-    def __init__(self, image_x, image_y, person=None):
-        self.location = (image_x, image_y)
+    def __init__(self, center_point, bbox, person=None):
+        self.location = center_point
+        self.bounding_box = bbox # [x,y,w,h]
         self.person = person
 
 
     def __repr__(self):
         return f"Spot at {self.location}. {self.person if self.person is not None else 'no one'} sits here."
 
+    def is_across(self, other, min_distance=800):
+        return abs(self.location[1] - other.location[1]) < self.bounding_box[3] and math.dist(self.location, other.location) < min_distance
