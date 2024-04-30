14fb833c443083684bf26dcbcf397257df302e4f
Uri Ziv <uriziv10@gmail.com>
2024-04-30 14:11:42 +0300

    cleanup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from werkzeug.utils import secure_filename
 from actions.indentifiers.indenty import sayHello
+from Table import Table
 
 # TODO hide behind a lock
 uid_counter = 1
@@ -8,6 +9,8 @@ uid_counter = 1
 app = Flask(__name__,
             static_folder='web/static',
             template_folder='web/template')
+
+THE_TABLE = None
 
 @app.route('/')
 def getHomePage():
@@ -23,13 +26,26 @@ def get_uid():
 
 @app.route('/upload_picture/', methods=['POST'])
 def upload_picture():
+    global THE_TABLE
     uploaded_picture = request.files.get('picture')
     if uploaded_picture and uploaded_picture.mimetype.startswith('image/'):
-        uploaded_picture.save(f'uploads//{secure_filename(uploaded_picture.filename)}')
-        return redirect('/', 200)
+        try:
+            filename = f'uploads//{secure_filename(uploaded_picture.filename)}'
+            uploaded_picture.save(filename)
+            THE_TABLE = Table(filename)
+            print(THE_TABLE)
+            return redirect('/table_details', 200)
+        except IOError:
+            return "<p> internal server file error :( </p>"
     
     return "internal error."
-    
+
+@app.route('/table_details', methods=['GET'])
+def table_details():
+    global THE_TABLE
+    return {"num people" : len(THE_TABLE.spots),
+            "spots": THE_TABLE.__repr__()}
+
 def main():
     app.run(debug=True)
 
