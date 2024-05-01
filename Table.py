bf9f2e4dd26ea96777a8c0e97c50ade35636a81e
MichalShechter <130558644+MichalShechter@users.noreply.github.com>
2024-05-02 00:52:18 +0300

    its all working!

--- Table.py
+++ Table.py
@@ -89,7 +89,10 @@ class Table(object):
 
     def populate_spots(self, names_ids, sitting_arrangement):
         for spot_index, id in enumerate(sitting_arrangement):
-            name = names_ids[id]
+            if names_ids.get(id):
+                name = names_ids[id]
+            else:
+                name = "no one"
             self.spots[spot_index].person = name
 
     def draw_sitting_arrangement(self, save_path):
@@ -105,7 +108,7 @@ class Table(object):
                 2,
             )
         try:
-            cv2.imwrite(orig, save_path)
+            cv2.imwrite(save_path, orig)
             return True
         except IOError:
             print(f"ERROR: failed to save result image to {save_path}")
