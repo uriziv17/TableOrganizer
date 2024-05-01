2f9ef29139e867f62ab99e2dd97a1935332a575d
uriziv17 <74453815+uriziv17@users.noreply.github.com>
2024-05-01 12:43:39 +0300

    table - draw result image

--- Table.py
+++ Table.py
@@ -17,7 +17,7 @@ class Table(object):
         self.image_path = image_path
         self.image = cv2.imread(image_path)
         self.scale = scale_by
-        self.image = image = cv2.resize(self.image, (0, 0), fx=scale_by, fy=scale_by)
+        self.image = cv2.resize(self.image, (0, 0), fx=scale_by, fy=scale_by)
         self.spots = self.extract_spots()
         self.relations = self.build_relations(self.spots)
 
@@ -29,7 +29,9 @@ class Table(object):
         here we will infer the locations of all the spots using cv2
         :return:
         """
-        found_contours = PlateDetector.detect_plates(self.image_path, scale_by=self.scale)
+        found_contours = PlateDetector.detect_plates(
+            self.image_path, scale_by=self.scale
+        )
         spots = {}
         for i in range(len(found_contours)):
             spots[i] = Spot(i, found_contours[i]["center"], found_contours[i]["bbox"])
@@ -46,7 +48,10 @@ class Table(object):
             distances.sort(key=lambda x: x[1])
             nearest_spot_1 = distances[0][0]
             nearest_spot_2 = distances[1][0]
-            relations[spot_index] = {nearest_spot_1: NEARBY_SCORE, nearest_spot_2: NEARBY_SCORE}
+            relations[spot_index] = {
+                nearest_spot_1: NEARBY_SCORE,
+                nearest_spot_2: NEARBY_SCORE,
+            }
 
             for other_index, _ in distances[2:]:
                 other = spots[other_index]
@@ -61,7 +66,15 @@ class Table(object):
         for spot_index, rels in self.relations.items():
             spot = self.spots[spot_index]
             cv2.circle(orig, spot.location, 5, PURPLE, cv2.FILLED)
-            cv2.putText(orig, f"{spot_index}", spot.location, cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 0, 0), 2)
+            cv2.putText(
+                orig,
+                f"{spot_index}",
+                spot.location,
+                cv2.FONT_HERSHEY_COMPLEX,
+                0.7,
+                (0, 0, 0),
+                2,
+            )
             for other_index, score in rels.items():
                 other = self.spots[other_index]
                 if score == NEARBY_SCORE:
@@ -73,3 +86,27 @@ class Table(object):
 
     def get_relation_between(self, spot_id_1, spot_id_2):
         return self.relations[spot_id_1][spot_id_2]
+
+    def populate_spots(self, names_ids, sitting_arrangement):
+        for spot_index, id in enumerate(sitting_arrangement):
+            name = names_ids[id]
+            self.spots[spot_index].person = name
+
+    def draw_sitting_arrangement(self, save_path):
+        orig = self.image.copy()
+        for index, spot in self.spots.items():
+            cv2.putText(
+                orig,
+                spot.person,
+                spot.location,
+                cv2.FONT_HERSHEY_COMPLEX,
+                0.7,
+                (0, 0, 0),
+                2,
+            )
+        try:
+            cv2.imwrite(orig, save_path)
+            return True
+        except IOError:
+            print(f"ERROR: failed to save result image to {save_path}")
+            return False
