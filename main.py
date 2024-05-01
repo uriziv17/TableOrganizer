24e4233ce7dc65ac3ad674046aabc28061035a63
uriziv17 <74453815+uriziv17@users.noreply.github.com>
2024-05-01 17:00:34 +0300

    forms test - upload not working

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template, request, redirect, url_for, jsonify
 from werkzeug.utils import secure_filename
 from Table import Table
 from actions.relaxtionLabel.relaxtion import updatePersonsMatrix, setPersonsMatrix, findSitting
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, FormField, SubmitField
+from wtforms.validators import DataRequired
 
 # TODO hide behind a lock
 uid_counter = 1
@@ -13,19 +17,34 @@ app = Flask(__name__,
 
 app.config['UPLOAD_FOLDER'] = os.path.join('web', 'static', 'uploads')
 
-THE_TABLE:Table = None
+THE_TABLE: Table = None
 names_dict = {}
 names_counter = 0
 person_matrix = None
+
+
+class PairForm(FlaskForm):
+    global names_dict
+    name1 = SelectField('Person 1', choices=[(i, name) for i, name in names_dict.items()], validators=[DataRequired()])
+    name2 = SelectField('Person 2', choices=[(i, name) for i, name in names_dict.items()], validators=[DataRequired()])
+    like = SelectField('Like', choices=[('yes', 'Yes'), ('no', 'No')], validators=[DataRequired()])
+
+
+class MainForm(FlaskForm):
+    pairs = FormField(PairForm)
+    submit = SubmitField('Submit')
+
+
 @app.route('/')
 def getHomePage():
     return render_template('index.html')
 
-@app.route('/get-uid/')
-def get_uid():
-    global uid_counter
-    uid_counter += 1
-    return {'uid': uid_counter - 1}
+
+# @app.route('/get-uid/')
+# def get_uid():
+#     global uid_counter
+#     uid_counter += 1
+#     return {'uid': uid_counter - 1}
 
 
 @app.route('/upload_picture/', methods=['POST'])
@@ -40,19 +59,23 @@ def upload_picture():
             THE_TABLE = Table(filename)
             return redirect('/add_names', 200)
         except IOError:
-            return "<p> internal server file error :( </p>"
-    
+            return {"message": "<p> internal server file error :( </p>"}
+
     return "internal error."
+
 
 @app.route('/table_details', methods=['GET'])
 def table_details():
     global THE_TABLE
-    #render_template('tableDetails.html', table=THE_TABLE)
-    return redirect('/add_names',200)
+    # render_template('tableDetails.html', table=THE_TABLE)
+    return redirect('/add_names', 200)
+
 
 def add_name_to_list(name):
     script = f"<script>addNameToList('{name}');</script>"
     return script
+
+
 @app.route('/add_names/', methods=['GET', 'POST'])
 def add_names():
     global names_counter, person_matrix
@@ -67,10 +90,10 @@ def add_names():
             # Call JavaScript function to add name to the list
             add_name_to_list(name)
             names_counter += 1
-            print (names_dict)
+            print(names_dict)
 
         if 'done' in request.form:  # Check if "Done" button is pressed
-            if len(names_dict)>len(THE_TABLE.spots):
+            if len(names_dict) > len(THE_TABLE.spots):
                 print("there are to many guests for this table")
                 names_dict.clear()
                 return redirect('/preferences')
@@ -78,23 +101,30 @@ def add_names():
             return redirect('/preferences', 200)  # Redirect to table details
         return redirect('/add_names')  # Redirect back to name collection page
 
+
 @app.route('/preferences', methods=['GET', 'POST'])
 def preferences():
     global names_dict, person_matrix
-    if request.method == 'GET':
-        return render_template('addCons.html', names=names_dict.values()) # Render seating preference page
-    else:
-        p1index = request.form.get('name1')
-        p2index = request.form.get('name2')
-        preference = request.form.get('preference')
-        print(p1index, " ", p2index, " ", preference)
-        if p1index == p2index:
-            return render_template('addCons.html', names=names_dict.values(), error="Please select two diffrent names")
-        else:
-            isLike = 'like' == preference
-            updatePersonsMatrix(person_matrix, p1index, p2index, isLike)
-            if 'done' in request.form:
-                findSitting(THE_TABLE, person_matrix)
+    form = MainForm()
+    if form.validate_on_submit():
+        # Process the data
+        pairs_data = [(pair['name1'], pair['name2'], pair['like']) for pair in form.pairs.data]
+        # Here you can do something with the pairs_data, like saving to a database
+        print(pairs_data)
+        return f'Data received: {pairs_data}'
+    return render_template('forms-test.html', form=form)
+        # p1index = request.form.get('name1')
+        # p2index = request.form.get('name2')
+        # preference = request.form.get('preference')
+        # print(p1index, " ", p2index, " ", preference)
+        # if p1index == p2index:
+        #     return render_template('addCons.html', names=names_dict.values(), error="Please select two diffrent names")
+        # else:
+        #     isLike = 'like' == preference
+        #     updatePersonsMatrix(person_matrix, p1index, p2index, isLike)
+        #     if 'done' in request.form:
+        #         findSitting(THE_TABLE, person_matrix)
+
 
 def main():
     app.run(debug=True)
